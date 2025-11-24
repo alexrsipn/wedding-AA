2ef92f010d4b038eab9b66fa7ed81c8317760a57
Alexis Ruiz <aruizs1501@alumno.ipn.mx>
2025-11-24 00:30:09 -0600

    FIX stable NextJS 14 version, ADD animated rotation mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,12 @@ export default function RootLayout({
       <body
         className={`${montserrat.variable} ${cinzel.variable} antialiased`}
       >
+      <div className="orientation-blocker">
+          <div className="phone-icon-animation">
+              <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"><rect x="5" y="2" width="14" height="20" rx="2" ry="2"></rect><line x1="12" y1="18" x2="12.01" y2="18"></line></svg>
+          </div>
+          <p className="mt-6 text-xl font-semibold">Por favor, gira tu dispositivo.</p>
+      </div>
       <Suspense fallback={<div>Cargando...</div>}>
           <AudioProvider>
               <GuestProvider>
